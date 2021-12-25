b29ce280956d6ef0df8208ed1eb4855738a666f9
Santosh Kumar Dash <santoshnet2016@gmail.com>
2021-12-25 12:12:26 +0530

    product screen update

--- src/screen/ProductView/index.js
+++ src/screen/ProductView/index.js
@@ -21,6 +21,8 @@ import Card from '../../components/Card';
 import MenuItem from '../Products/Component/MenuItem';
 import {BASE_URL} from './../../axios/API';
 import TextViewMedium from './../../components/CustomText/TextViewMedium';
+import {addCart,updateCart,getUserCart} from './../../axios/ServerRequest';
+import {getUserDetails,getCart,setCart} from '../../utils/LocalStorage';
 
 class Index extends Component {
   constructor(props) {
@@ -28,19 +30,102 @@ class Index extends Component {
     this.state = {
       productData: '',
       selectedItem: [],
-      counter: '',
+      quantity: 0,
+      user:null,
+      cartCount:0,
+      cart:[]
     };
   }
 
-  componentDidMount() {
-    this.setState({productData: this.props.route.params.item});
+  async componentDidMount() {
+    this._unsubscribe = this.props.navigation.addListener('focus', () => {
+      console.log('refresh');
+    });
+    const user = await getUserDetails();
+    const cart = await getCart();
+    this.setState({productData: this.props.route.params.item,user:user,cart:cart, cartCount:cart.length});
+    this.getCartData();
+
   }
 
- 
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+
+
+addToCart=(param)=>{
+  addCart(this.state.user.token,param.id,param.quantity)
+      .then(response => {
+        let data = response.data;
+        if (data.status === 200) {
+          this.getCartData();
+        } else {
+          Toast.show(data.message, Toast.LONG);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+
+}
+updateToCart=(param)=>{
+  updateCart(this.state.user.token,param.id,param.quantity)
+  .then(response => {
+    let data = response.data;
+    if (data.status === 200) {
+      console.log(data.message);
+     
+    } else if(data.status === 204){
+      
+      this.setState({quantity:this.state.quantity-1});
+    }else {
+      Toast.show(data.message, Toast.LONG);
+    }
+    this.getCartData();
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+}
+
+setQuantity=()=>{
+  console.log(this.state.cart);
+   let data= this.state.cart.find(item1=>item1.product_id==this.state.productData.id);
+   if(data){
+     this.setState({quantity:data.quantity})
+  }else{
+    this.setState({quantity:0})
+
+  }
+
+}
+
+getCartData=()=>{
+  getUserCart(this.state.user.token)
+      .then(response => {
+        let data = response.data;
+        if (data.status === 200) {
+          this.setState({cart:data.cart, cartCount:data.cart.length});
+          setCart(data.cart);
+          this.setQuantity();
+        } else {
+          Toast.show(data.message, Toast.LONG);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+}
+
+
+
+
 
 
   render() {
-      const {counter} = this.state;
+      const {quantity,productData} = this.state;
     return (
       <Column style={{backgroundColor: Color.white, flex: 1}}>
         <AppStatusBar
@@ -50,7 +135,7 @@ class Index extends Component {
         />
         <RelativeLayout style={{marginTop: -25}}>
           <Image
-            source={{uri: BASE_URL + this.state.productData.image}}
+            source={{uri: BASE_URL + productData.image}}
             style={{width: Dimension.window.width, height: 270}}
           />
           <AbsoluteLayout
@@ -78,34 +163,34 @@ class Index extends Component {
             marginTop: -30,
           }}>
           <Column style={{padding: 20}}>
-            <Text style={styles.title}>{this.state.productData.name}</Text>
+            <Text style={styles.title}>{productData.name}</Text>
             <Text style={styles.subTitle}>
-              {this.state.productData.category}
+              {productData.category}
             </Text>
             <Row style={{marginTop: 10}}>
               <Row style={styles.optionContainer}>
                 <Icon name={'star'} color={Color.yellow} size={24} />
                 <Text style={styles.optionText}>
-                  {this.state.productData.rating}
+                  {productData.rating}
                 </Text>
               </Row>
               <Row
                 style={[styles.optionContainer, {backgroundColor: '#e2e7ff'}]}>
                 <Icon name={'clock-o'} color={Color.blue} size={24} />
                 <Text style={styles.optionText}>
-                  {this.state.productData.prepareTime}
+                  {productData.prepareTime}
                 </Text>
               </Row>
 
               <Row>
-                {counter == 0 ? (
+                {quantity == 0 ? (
                   <TouchableOpacity
                     style={{
                       padding: 10,
                       borderRadius: 20,
                       backgroundColor: Color.colorPrimary,
                     }}
-                    onPress={() => setCounter(counter + 1)}>
+                    onPress={() => this.addToCart({quantity:parseInt(quantity)+1,id:productData.id})}>
                     <FeatherIcon name={'plus'} size={14} color={Color.white} />
                   </TouchableOpacity>
                 ) : (
@@ -116,7 +201,7 @@ class Index extends Component {
                         borderRadius: 20,
                         backgroundColor: Color.colorPrimary,
                       }}
-                      onPress={() => setCounter(counter - 1)}>
+                      onPress={() => this.updateToCart({quantity:parseInt(quantity)-1,id:productData.id})}>
                       <FeatherIcon
                         name={'minus'}
                         size={14}
@@ -128,7 +213,7 @@ class Index extends Component {
                         styles.optionText,
                         {width: 27, textAlign: 'center'},
                       ]}>
-                      {counter}
+                      {quantity}
                     </Text>
                     <TouchableOpacity
                       style={{
@@ -137,7 +222,7 @@ class Index extends Component {
                         marginLeft: 10,
                         backgroundColor: Color.colorPrimary,
                       }}
-                      onPress={() => setCounter(counter + 1)}>
+                      onPress={() => this.updateToCart({quantity:parseInt(quantity)+1,id:productData.id})}>
                       <FeatherIcon
                         name={'plus'}
                         size={14}
@@ -149,15 +234,11 @@ class Index extends Component {
               </Row>
             </Row>
             <TextViewMedium style={{marginTop: 20}}>
-              {this.state.productData.description}
+              {productData.description}
             </TextViewMedium>
           </Column>
         </Column>
-        <FlatList
-          data={this.state.productData.menu}
-          renderItem={this.renderItem}
-          keyExtractor={item => item.id}
-        />
+       
       </Column>
     );
   }
